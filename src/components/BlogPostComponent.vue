03e7f7ed39f9e35f5a6a6e9839d55fadc8d95bc0
AndiOrler <andreas.orler@gmail.com>
2023-11-28 14:06:33 +0100

    BlogPostComponent added

---
+++ src/components/BlogPostComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <div class="d-flex align-items-center">
+        <h3 class="card-title">{{ props.title }}</h3>
+        <p class="ms-auto lh-1">{{ postDate }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  title: {
+    type: String
+  }
+})
+
+const postDate = new Date().toDateString()
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,12 +1,39 @@
+import BlogPostComponent from '@/components/BlogPostComponent.vue';
 <template>
-  <PageHeader/>
+  <PageHeader />
   <div class="page-body">
     <div class="row">
-     <p>
-      Some Content coming here!!!!
-     </p>
+      <div v-for="post in myPosts" :key="post.id">
+        <BlogPostComponent :title="post.title"></BlogPostComponent>
+      </div>
     </div>
-    
-
   </div>
 </template>
+
+<script setup lang="ts">
+import { onBeforeMount } from 'vue'
+
+const myPosts = [
+  {
+    id: 1,
+    title: 'fooo',
+    text: 'lorem ipsum'
+  }
+]
+
+onBeforeMount(() => {
+  fetchPosts()
+})
+
+const fetchPosts = async () => {
+  try {
+    console.log('home page - trying to fetch posts')
+
+    const response = await fetch('/posts.json')
+    const data = await response.json()
+    myPosts.push(data)
+  } catch (error) {
+    console.error('home page - error fetching posts:', error)
+  }
+}
+</script>
